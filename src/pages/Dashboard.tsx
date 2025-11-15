cbbbf7fe2081a0ab724b659f3e1b236117c3fc92
telkomdev-rikiramadhani <rikiramadhani08@gmail.com>
2025-11-15 18:12:52 +0700

    fix format

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -83,7 +83,7 @@ export default function Dashboard() {
                 amount: 0,
                 description:
                   `Hai! Servernya lagi offline dulu ya 😊 \n
-                   Coba lagi di jam operasional: Senin-Jumat 09.00–17.00.`,
+                   Coba lagi di jam operasional: \nSenin-Jumat 09.00–17.00.`,
                 created_at: new Date().toISOString()
               }
             ]
